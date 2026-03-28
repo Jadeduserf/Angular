f637a0fc7660417fb02482642cc9f67f2840c2df
Diogo Moreno <diogomoreno574@gmail.com>
2026-03-28 15:25:01 +0000

    Services with signals

--- 01-starting-project/src/app/user-input/user-input.component.ts
+++ 01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,7 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import type { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -12,22 +11,25 @@ import type { InvestmentInput } from '../investment-input.model';
 })
 export class UserInputComponent {
 
-  calculate = output<InvestmentInput>();
+  
 
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
   enteredExpectedReturn = signal('5');
   enteredDuration = signal('10');
 
+  constructor(private investmentService: InvestmentService) {}
+
 
   // This method will be called when the form is submitted
   onSubmit(){
-    this.calculate.emit({
-      initialInvestment: +this.enteredInitialInvestment(),
+    this.investmentService.calculateInvestmentResults(
+      {initialInvestment: +this.enteredInitialInvestment(),
       annualInvestment: +this.enteredAnnualInvestment(),
       expectedReturn: +this.enteredExpectedReturn(),
       duration: +this.enteredDuration()
-    });
+      });
+      
     this.enteredInitialInvestment.set('0');
     this.enteredAnnualInvestment.set('0');
     this.enteredExpectedReturn.set('5');
